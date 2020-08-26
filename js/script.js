ab66a034e96a8bf7472210509787527096a8d2fb
FrancescoLunesu <francesco.lunesu@outlook.it>
2020-08-26 15:42:15 +0200

    Secondo commit: array di objs e ciclare stampando nome e cognome

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // PRIMA PARTE:
-
 // Creare un oggetto che descriva
 // uno studente con le seguenti proprietà: nome, cognome ed età.
 
@@ -12,4 +11,30 @@ var studente = {
 // Stampare a schermo attraverso il for in tutte le proprietà.
 for (var key in studente){
     console.log(studente[key]);
+}
+
+
+// SECONDA PARTE:
+// Creare un array di oggetti di studenti.
+
+var studenti = [
+    {
+        "nome" : 'Mirco',
+        "cognome" : 'Rossi',
+    },
+    {
+        "nome" : 'Luca',
+        "cognome" : 'Bianchini',
+    },
+    {
+        "nome" : 'Matteo',
+        "cognome" : 'Matteucci',
+    },
+];
+
+// Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
+
+for (var i = 0; i <studenti.length; i++){
+  console.log(studenti[i].nome);
+  console.log(studenti[i].cognome);
 }
